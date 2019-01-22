c45371ebea6eb2bfb047d949ea9c298cb6b1f954
Leon-Arno Rimrod <leonarno.rimrod@gmail.com>
2019-01-22 16:50:28 +0100

    Created Contact-Manager model

---
+++ model/Contact.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const trimmedString = options => ({
+  type: String,
+  trim: true,
+  ...options
+});
+
+const nameSchema = new Schema(
+  {
+    first_name: trimmedString({ required: true }),
+    last_name: trimmedString()
+  },
+  { _id: false }
+);
+
+const addressSchema = new Schema(
+  {
+    home_address: trimmedString({ required: true }),
+    work_address: trimmedString()
+  },
+  { _id: false }
+);
+
+const phoneNumberSchema = new Schema(
+  {
+    mobile_phone: trimmedString({ required: true }),
+    home_phone: trimmedString()
+  },
+  { _id: false }
+);
+
+const ContactSchema = new Schema({
+  name: { type: nameSchema },
+  address: { type: addressSchema },
+  phone_number: { type: phoneNumberSchema },
+  email: { type: String, default: 'No entry' },
+  website: { type: String, default: 'No entry' }
+});
+
+module.exports = mongoose.model('Contact', ContactSchema);

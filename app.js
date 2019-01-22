3f9af1adde5ba65985fee179402a2e904a733669
Leon-Arno Rimrod <leonarno.rimrod@gmail.com>
2019-01-22 15:14:35 +0100

    Initiall commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const contactRouter = require('./routes/contacts');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use('/', indexRouter);
+app.use('/contacts', contactRouter);
+
+module.exports = app;

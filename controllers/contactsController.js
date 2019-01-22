7f4c28030fa7a8f92328aa7ce9c9b19691f4a104
Leon-Arno Rimrod <leonarno.rimrod@gmail.com>
2019-01-22 17:15:41 +0100

    Created Contacts-Controller

---
+++ controllers/contactsController.js
@@ -0,0 +1,28 @@
+const Contact = require('../model/Contact');
+
+exports.listContacts = async (req, res) => {
+  res.json(await Contact.find());
+};
+
+exports.addContact = async (req, res) => {
+  const contact = new Contact(req.body);
+  await contact.save();
+  res.json(contact);
+};
+
+exports.getContact = async (req, res) => {
+  const contact = await contact.findById(req.params.id);
+  res.json(contact);
+};
+
+exports.updateContact = async (req, res) => {
+  const contact = await contact.findByIdAndUpdate(req.params.id, req.body, {
+    new: true
+  });
+  res.json(contact);
+};
+
+exports.deleteContact = async (req, res) => {
+  const contact = await contact.findByIdAndDelete(req.params.id);
+  res.json(contact);
+};
